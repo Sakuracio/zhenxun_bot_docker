244cd19e69bec8f2b6ccf6ce604a11cd7df4b5a2
Sakuracio <66161166+Sakuracio@users.noreply.github.com>
2021-12-30 17:57:24 +0800

    update file

--- zhenxun_bot/configs/config.py
+++ zhenxun_bot/configs/config.py
@@ -9,7 +9,7 @@ NICKNAME: str = "小真寻"
 # 数据库（必要）
 # 如果填写了bind就不需要再填写后面的字段了#）
 # 示例："bind": "postgresql://user:password@127.0.0.1:5432/database"
-bind: str = ""  # 数据库连接链接
+bind: str = "postgresql://zhenxun:zhenxun@0.0.0.0:5432/zhenxundata"  # 数据库连接链接
 sql_name: str = "postgresql"
 user: str = ""  # 数据用户名
 password: str = ""  # 数据库密码
